90aafd7098169c3b95861ae0c832531d07cc92b5
faxila <faxila3@gmail.com>
2022-04-14 18:00:09 +0300

    added logic social

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+let url = encodeURIComponent(location.href);
+let title = encodeURIComponent(document.title);
+title = title.replace(/\'/g, '%27');
+
+let facebook = document.getElementById('social-facebook');
+let attributeValueFacebook = "window.open(\'//www.facebook.com/sharer/sharer.php?u=" + url + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false";
+facebook.setAttribute('onclick', attributeValueFacebook);
+
+let linkedin = document.getElementById('social-linkedin');
+let attributeValueLinkedin = "window.open(\'//www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=600, height=400, toolbar=0, status=0\');return false";
+linkedin.setAttribute('onclick', attributeValueLinkedin);
