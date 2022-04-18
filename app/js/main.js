ef20f6548782d7b05e912f228f2113f5f7e66064
faxila <faxila3@gmail.com>
2022-04-18 13:16:26 +0300

    fix url

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,9 @@ let title = encodeURIComponent(document.title);
 title = title.replace(/\'/g, '%27');
 
 let facebook = document.getElementById('social-facebook');
-let attributeValueFacebook = "window.open(\'//www.facebook.com/sharer/sharer.php?u=" + url + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false";
+let attributeValueFacebook = "window.open(\'https://www.facebook.com/sharer/sharer.php?u=" + url + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false";
 facebook.setAttribute('onclick', attributeValueFacebook);
 
 let linkedin = document.getElementById('social-linkedin');
-let attributeValueLinkedin = "window.open(\'//www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=600, height=400, toolbar=0, status=0\');return false";
+let attributeValueLinkedin = "window.open(\'https://www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title + "\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=600, height=400, toolbar=0, status=0\');return false";
 linkedin.setAttribute('onclick', attributeValueLinkedin);
