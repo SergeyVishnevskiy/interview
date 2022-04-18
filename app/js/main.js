f61312cabc9755c7a18b10ba73edfc4fedd66c28
faxila <faxila3@gmail.com>
2022-04-18 13:05:39 +0300

    Hot Fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-let url = encodeURIComponent(location.href);
+let url = encodeURIComponent('https://lizard-soft.com/stories/holub/');
 let title = encodeURIComponent(document.title);
 title = title.replace(/\'/g, '%27');
 
