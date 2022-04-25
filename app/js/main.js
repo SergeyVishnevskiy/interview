05d66f061018f1610273aebbb0f0d8091d038387
SergeyVishnevskiy <serrzik13manager@gmail.com>
2022-04-25 15:27:43 +0300

    add link

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-let url = encodeURIComponent('https://lizard-soft.com/stories/maksym/');
+let url = encodeURIComponent('https://lizard-soft.com/stories/lera2/');
 let title = encodeURIComponent(document.title);
 title = title.replace(/\'/g, '%27');
 
